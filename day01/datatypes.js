b9c07a455f0ef23c409479687126585e87f73960
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2021-02-01 22:21:58 +0500

    day01

---
+++ day01/datatypes.js
@@ -0,0 +1,45 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let address = '123 abc town xyz country';
+let doILoveJS = true;
+let zipcode = undefined;
+let framework = null;
+
+console.log(typeof address);
+console.log(typeof doILoveJS);
+console.log(typeof zipcode);
+console.log(typeof framework);
+
+// 6. Declare four variables without assigning values
+
+let a;
+let b;
+let c;
+let d;
+
+// 7. Declare four variables with assigned values
+
+let e = 234;
+let f = "some string";
+let g = true;
+let h = 34.5;
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let fName;
+let lName;
+let mStatus;
+let your_country;
+let your_age;
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName, lastName, maritalStatus, country, age;
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 78;
+let yourAge = 89;
+
+console.log("I am "+ myAge + " years old.");
+console.log("You are "+ yourAge + " years old.");
---
+++ day01/variable.js
@@ -0,0 +1,7 @@
+
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let address = '123 abc town xyz country';
+let doILoveJS = true;
+let zipcode = undefined;
+let framework = null;
